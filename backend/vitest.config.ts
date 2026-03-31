fc6a0fafc5ef2196b3491c625d5c4be6be3b4aa3
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2026-03-31 17:56:43 +0100

    docs: add comments to backend/vitest.config.ts

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -4,3 +4,9 @@ import {
   getClarinetVitestsArgv,
 } from "@stacks/clarinet-sdk/vitest";
 
+/*
+  In this file, Vitest is configured so that it works seamlessly with Clarinet and the Simnet.
+
+  The `vitest-environment-clarinet` will initialise the clarinet-sdk
+  and make the `simnet` object available globally in the test files.
+

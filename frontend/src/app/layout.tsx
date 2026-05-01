644a3cedaf59b445a24a6c6be2cee0efbf582015
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2026-05-01 19:36:04 +0100

    feat: add logic to frontend/src/app/layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,3 +32,9 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>
+        <WalletProvider>
